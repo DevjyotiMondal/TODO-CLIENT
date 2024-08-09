c9e1a10a4c6cac3e93a2030b78c226af203a032f
DevjyotiMondal <devjyotimondal070802@gmail.com>
2024-08-09 23:29:16 +0530

    fixed everything

--- src/Helpers/axios.js
+++ src/Helpers/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 // const BASE_URL ="http://localhost:5000/api/v1"
-const BASE_URL="https://todo-server-6n7k.onrender.com"
+const BASE_URL="https://todo-server-6n7k.onrender.com/api/v1"
 
 const axiosInstance =axios.create(
     {
